7a4ce8c7e0bd583bbbfe7be5c070a9d82f74796d
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2023-07-10 17:36:13 +1000

    today ui

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var date = moment();
+getCity();
 
-var currentDate = date.format('dddd D MMMM');
+const date = moment();
+const currentDate = date.format("dddd D MMMM");
 
-document.getElementById("today").innerHTML = currentDate; 
+document.getElementById("today").innerHTML = currentDate;
+
+async function getCity() {
+    const cityres = await fetch(
+        "https://geolocation-db.com/json/"
+    );
+    const city = await cityres.json();
+
+    document.getElementById("today-temp").innerHTML = city.city;
+}
+
