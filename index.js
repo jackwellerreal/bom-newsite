5cdb2d28ee169196f4c225bd48c67ae0547eb3e0
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2023-07-11 17:14:40 +1000

    rename data p1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-setWeather();
+setTodayWeather("Brisbane"); // Make sure it has a Capital Letter at the start
+setTodayCities();
 
 const time = moment();
 const currentTime = time.format("HH:ss, dddd D MMMM");
@@ -6,11 +7,62 @@ const currentTime = time.format("HH:ss, dddd D MMMM");
 document.getElementById("today-time").innerHTML = currentTime;
 document.getElementById("today-city").innerHTML = "Brisbane";
 
-async function setWeather() {
+async function setTodayWeather(city) {
     const weatherres = await fetch(
-        "http://api.weatherapi.com/v1/current.json?key=&q=Brisbane&aqi=no"
+        `https://what-question-mark.github.io/bom-newsite/data/${city}.json`
     );
     const weather = await weatherres.json();
 
-    document.getElementById("today-temp").innerHTML = `${weather.current.temp_c}°C`;
+    document.getElementById(
+        "today-temp"
+    ).innerHTML = `${weather.current.temp_c}°C`;
+    document.getElementById(
+        "today-temp-feels"
+    ).innerHTML = `Feels like ${weather.current.feelslike_c}°C`;
+
+    document.getElementById(
+        "today-wind"
+    ).innerHTML = `${weather.current.wind_kph} kph (${weather.current.wind_dir})`;
+    document.getElementById(
+        "today-humidity"
+    ).innerHTML = `${weather.current.humidity}%`;
+    document.getElementById(
+        "today-percipitation"
+    ).innerHTML = `${weather.current.precip_mm}mm`;
+    document.getElementById(
+        "today-gust"
+    ).innerHTML = `${weather.current.gust_kph} kph`;
+    document.getElementById("today-uv").innerHTML = `${parseFloat(
+        weather.current.uv
+    )}`;
+    document.getElementById(
+        "today-pressure"
+    ).innerHTML = `${weather.current.pressure_mb} mb`;
+}
+
+async function setTodayCities() {
+    const cities = [
+        "sydney",
+        "melbourne",
+        "brisbane",
+        "perth",
+        "adelaide",
+        "hobart",
+        "canberra",
+        "darwin",
+    ];
+
+    for (let city of cities) {
+        const weatherres = await fetch(
+            `https://what-question-mark.github.io/bom-newsite/data/${city}.json`
+        );
+        const weather = await weatherres.json();
+
+        document.getElementById(
+            `today-other-${city}-temp`
+        ).innerHTML = `${weather.current.temp_c}°C`;
+        document.getElementById(
+            `today-other-${city}-wind`
+        ).innerHTML = `${weather.current.wind_kph}kph (${weather.current.wind_dir})`;
+    }
 }
